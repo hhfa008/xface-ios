25317fd197e2d4604b16908446cccb8e9c153c3d
liumn <liumn@polyvi.com>
2013-09-17 17:22:38 +0800

    Added xFaceLib

---
+++ xFaceLib/xFaceLibTests/xFaceLibTests.h
@@ -0,0 +1,13 @@
+//
+//  xFaceLibTests.h
+//  xFaceLibTests
+//
+//  Created by zoe on 13-9-17.
+//  Copyright (c) 2013年 Polyvi Inc. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface xFaceLibTests : SenTestCase
+
+@end
---
+++ xFaceLib/xFaceLibTests/xFaceLibTests.m
@@ -0,0 +1,32 @@
+//
+//  xFaceLibTests.m
+//  xFaceLibTests
+//
+//  Created by zoe on 13-9-17.
+//  Copyright (c) 2013年 Polyvi Inc. All rights reserved.
+//
+
+#import "xFaceLibTests.h"
+
+@implementation xFaceLibTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in xFaceLibTests");
+}
+
+@end
