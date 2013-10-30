0e101ad72871392a96beb1e6eab36cb964217f13
liumn <liumn@polyvi.com>
2013-10-30 16:20:35 +0800

    [Doing-193]Sync cordova-ios bin(9a63bb79d)
    CB-5217: cordova emulate ios doesn't exit
    CB-4805 - Update cordova/run and cordova/lib/install-device to use
    latest ios-deploy for iOS 7
    CB-5103 Fix cordova/run: --emulate should be --emulator (fix CLI usage)
    CB-4872 - need to copy over scipts to project
    [CB-4872] - added iOS sdk version scripts
    CB-5099 - Add missing icons especially iOS 7 120x120 icon to default
    template
    CB-4990 - Can't run emulator from cordova cli
    CB-4978 - iOS - Remove HideKeyboardFormAccessoryBar and
    KeyboardShrinksView preferences in config.xml
    [CB-4799] Add update script for iOS.

--- bin/templates/project/__TESTING__/Classes/AppDelegate.h
+++ bin/templates/project/__TESTING__/Classes/AppDelegate.h
@@ -19,7 +19,7 @@
 
 //
 //  AppDelegate.h
-//  player
+//  __TESTING__
 //
 //  Created by ___FULLUSERNAME___ on ___DATE___.
 //  Copyright ___ORGANIZATIONNAME___ ___YEAR___. All rights reserved.
@@ -34,7 +34,7 @@
 @interface AppDelegate : NSObject <UIApplicationDelegate>{}
 
 // invoke string is passed to your app on launch, this is only valid if you
-// edit player-Info.plist to add a protocol
+// edit __TESTING__-Info.plist to add a protocol
 // a simple tutorial can be found here :
 // http://iphonedevelopertips.com/cocoa/launching-your-own-application-via-a-custom-url-scheme.html
 
